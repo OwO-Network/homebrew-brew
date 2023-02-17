86e45a4b147738b11226295578c8253463e095e0
Vincent Young <missuo@pm.me>
2023-02-17 19:29:50 +0800

    fix: shell script

--- getRelease.sh
+++ getRelease.sh
@@ -3,7 +3,7 @@
  # @Author: Vincent Young
  # @Date: 2023-02-17 19:18:08
  # @LastEditors: Vincent Young
- # @LastEditTime: 2023-02-17 19:23:31
+ # @LastEditTime: 2023-02-17 19:29:41
  # @FilePath: /homebrew-brew/getRelease.sh
  # @Telegram: https://t.me/missuo
  # 
@@ -12,5 +12,5 @@
 
 update_chatgpt(){
     last_version=$(curl -Ls "https://api.github.com/repos/lencx/ChatGPT/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/v//g')
-    sed -i "s/version.*/version "${last_version}\"/g" Casks/chatgpt.rb
+    sed -i "s/version.*/version \"${last_version}\"/g" Casks/chatgpt.rb
 }
