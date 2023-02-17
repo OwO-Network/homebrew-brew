fb4ce673b1a193dab9954de929a4aa7a3992da36
missuo <missuo@users.noreply.github.com>
2023-02-17 11:31:34 +0000

    Update ChatGPT release version

--- Casks/chatgpt.rb
+++ Casks/chatgpt.rb
@@ -1,12 +1,12 @@
 cask "chatgpt" do
-  version "0.10.0"
+  version "0.11.0"
   arch = Hardware::CPU.arch.to_s
   sha256s = {
     "x86_64" => "5f8013bee34daa53be8612b751955f745e7af9ef85b3541eba304b45176b6d8a",
     "aarch64" => "a5d914277d16827c5e3c641abd80c7978f78b8ccf36bf08661e1bc06efc6224e"
   }
   if arch == "arm64" then arch = "aarch64" end
-  url "https://github.com/lencx/ChatGPT/releases/download/v#{version}/ChatGPT_#{version}_macos_#{arch}.dmg"
+  url "https://github.com/lencx/ChatGPT/releases/download/v#{version "0.11.0"
   sha256 sha256s[arch]
 
   name "ChatGPT"
