90ee56c0dd2d264e143f94f50f87114387db834b
Vincent Young <missuo@pm.me>
2023-03-03 01:06:27 +0800

    fix: sha256

--- getRelease.sh
+++ getRelease.sh
@@ -3,22 +3,21 @@
  # @Author: Vincent Young
  # @Date: 2023-02-17 19:18:08
  # @LastEditors: Vincent Young
- # @LastEditTime: 2023-03-02 23:36:42
+ # @LastEditTime: 2023-03-03 01:05:20
  # @FilePath: /homebrew-brew/getRelease.sh
  # @Telegram: https://t.me/missuo
  # 
  # Copyright © 2023 by Vincent, All Rights Reserved. 
 ### 
 
-update_chatgpt(){
-    last_version=$(curl -Ls "https://api.github.com/repos/lencx/ChatGPT/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/v//g')
-    sed -i "s/version.*/version \"${last_version}\"/g" Casks/chatgpt.rb
-}
-
 update_deeplx(){
     last_version=$(curl -Ls "https://api.github.com/repos/OwO-Network/DeepLX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/v//g')
     sed -i "s/version \".*/version \"${last_version}\"/g" Formula/deeplx.rb
+    wget https://github.com/OwO-Network/DeepLX/releases/download/v${last_version}/deeplx_darwin_amd64
+    wget https://github.com/OwO-Network/DeepLX/releases/download/v${last_version}/deeplx_darwin_arm64
+    amd64_sha256=$(sha256sum deeplx_darwin_amd64 | cut -d ' ' -f 1)
+    arm64_sha256=$(sha256sum deeplx_darwin_arm64 | cut -d ' ' -f 1)
+    sed -i '' '0,/sha256 \"[0-9a-f]*\"/s//sha256 \"${arm64_sha256}\"/' Formula/deeplx.rb
+    sed -i '' '1,/sha256 \"[0-9a-f]*\"/s//sha256 \"${amd64_sha256}\"/' Formula/deeplx.rb
 }
-
-update_chatgpt
 update_deeplx
