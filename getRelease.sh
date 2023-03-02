d47d5af3e4f3ac43c336d6006a04e1f2760c1864
Vincent Young <missuo@pm.me>
2023-03-03 01:35:00 +0800

    test

--- getRelease.sh
+++ getRelease.sh
@@ -3,7 +3,7 @@
  # @Author: Vincent Young
  # @Date: 2023-02-17 19:18:08
  # @LastEditors: Vincent Young
- # @LastEditTime: 2023-03-03 01:23:55
+ # @LastEditTime: 2023-03-03 01:34:44
  # @FilePath: /homebrew-brew/getRelease.sh
  # @Telegram: https://t.me/missuo
  # 
@@ -12,9 +12,9 @@
 
 update_deeplx(){
     last_version=$(curl -Ls "https://api.github.com/repos/OwO-Network/DeepLX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/v//g')
-    sed -i "s/version \".*/version \"${last_version}\"/g" Formula/deeplx.rb
-    wget https://github.com/OwO-Network/DeepLX/releases/download/v${last_version}/deeplx_darwin_amd64
-    wget https://github.com/OwO-Network/DeepLX/releases/download/v${last_version}/deeplx_darwin_arm64
+    sed -i "s/version \".*\"/version \"${last_version}\"/g" Formula/deeplx.rb
+    wget -O deeplx_darwin_amd64 https://github.com/OwO-Network/DeepLX/releases/download/v${last_version}/deeplx_darwin_amd64
+    wget -O deeplx_darwin_arm64 https://github.com/OwO-Network/DeepLX/releases/download/v${last_version}/deeplx_darwin_arm64
     amd64_sha256=$(sha256sum deeplx_darwin_amd64 | cut -d ' ' -f 1)
     arm64_sha256=$(sha256sum deeplx_darwin_arm64 | cut -d ' ' -f 1)
     sed -i "0,/sha256 \".*\"/s//sha256 \"$arm64_sha256\"/" Formula/deeplx.rb
