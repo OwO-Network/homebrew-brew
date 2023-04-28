a5e356240f8939709fa52930a55bcbee7de5c0ce
Vincent Young <missuo@pm.me>
2023-04-28 23:49:26 +0800

    update sha256

--- Casks/xiaobaiyang.rb
+++ Casks/xiaobaiyang.rb
@@ -2,14 +2,14 @@ cask "xiaobaiyang" do
     version "3.10.25"
     arch = Hardware::CPU.arch.to_s
     sha256s = {
-      "x86_64" => "1d9102a4384b5e3c68951410f4bb1c4bf905179c8b8b48e4bea32dc54b9b1ec1",
-      "aarch64" => "29d8135c1c8e754d0c17349dcc1714f35259be78e51f18dde95e08166dab30c6"
+      "x86_64" => "245c8afa6c850e4359b27cd11233ccd137e023c74cf7cb294ba26056049527ee",
+      "aarch64" => "4d4cb73d89bc04165e585b39a6818f02d691858571d16ee8df81400b74873d75"
     }
     if arch == "arm64" then arch = "aarch64" end
     url "https://github.com/missuo/AliyunPanMac/releases/download/v#{version}/AliyunPan-v#{version}-#{arch}.dmg"
     sha256 sha256s[arch]
     name 'xiaobaiyang'
-    homepage 'https://github.com/missuo/AliyunPanMac/'
+    homepage 'https://github.com/missuo/AliyunPanMac'
 
     app "阿里云盘.app"
   end
