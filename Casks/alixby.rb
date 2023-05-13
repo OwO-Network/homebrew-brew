86ec2710640a2a454dc32939ed9f5ee49809afa2
Vincent Young <missuo@pm.me>
2023-05-13 18:58:22 +0800

    chore: update alixby to v3.10.36

--- Casks/alixby.rb
+++ Casks/alixby.rb
@@ -1,9 +1,9 @@
 cask "alixby" do
-    version "3.10.25"
+    version "3.10.26"
     arch = Hardware::CPU.arch.to_s
     sha256s = {
-      "x86_64" => "245c8afa6c850e4359b27cd11233ccd137e023c74cf7cb294ba26056049527ee",
-      "aarch64" => "4d4cb73d89bc04165e585b39a6818f02d691858571d16ee8df81400b74873d75"
+      "x86_64" => "d55d1a7bd4b43feebf84f5e10147f951f8268b4a88f242f9ddeb1106a03d6b8d",
+      "aarch64" => "e8357915856f77c18f77131b77892a7fd24ec6ac5bce8ca889ab3d5a15a852b9"
     }
     if arch == "arm64" then arch = "aarch64" end
     url "https://github.com/missuo/AliyunPanMac/releases/download/v#{version}/AliyunPan-v#{version}-#{arch}.dmg"
