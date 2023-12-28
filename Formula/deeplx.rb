c06e486f6296401b388d7ab22596db6beaa022f8
Vincent Young <missuo@pm.me>
2023-12-28 16:06:11 +0800

    Update deeplx.rb

--- Formula/deeplx.rb
+++ Formula/deeplx.rb
@@ -1,8 +1,8 @@
 class Deeplx < Formula
-  desc "DeepLX is an permanently free DeepL API written in Golang."
+  desc "DeepLX is a permanently free DeepL API client written in Golang."
   homepage "https://github.com/OwO-Network/DeepLX"
   version "0.8.8"
-  
+
   if Hardware::CPU.arm?
     url "https://github.com/OwO-Network/DeepLX/releases/download/v#{version}/deeplx_darwin_arm64"
     sha256 "dd2a8fe14a5833301f88d27821a1e9d4348ee0c7d71f7bbe1d134cf34e005c32"
@@ -11,49 +11,61 @@ class Deeplx < Formula
     sha256 "8fb5eac72356151f7c45bb61e74d722b6fb67134201757e7948740d36b41d482"
   end
 
-  depends_on "daemon"
-
   def install
     bin.install Dir["deeplx_*"].first => "deeplx"
-  end
-
-  def post_install
-    (var/"log/deeplx").mkpath
-    (var/"run/deeplx").mkpath
-    (etc/"deeplx/plugins").mkpath
-    install_startup_script
+    (var/"deeplx").mkpath
+    (var/"log").mkpath
   end
 
   def uninstall
-    post_uninstall
+    system "brew", "services", "stop", "#{name}" if (var/"log/deeplx.log").exist?
+    (var/"log/deeplx.log").unlink if (var/"log/deeplx.log").exist?
+    (var/"deeplx").rmtree if (var/"deeplx").exist?
     super
   end
 
   def post_uninstall
-    system "#{HOMEBREW_PREFIX}/bin/brew", "services", "stop", "deeplx"
-    (etc/"rc.d/deeplx").unlink if (etc/"rc.d/deeplx").exist?
+    system "rm", "-rf", "/opt/homebrew/etc/deeplx"
   end
+
+def plist
+  <<~XML
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/deeplx</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/deeplx.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/deeplx.log</string>
+    </dict>
+    </plist>
+  XML
+end
 
   def caveats
     <<~EOS
-      To start deeplx manually, run:
-        sudo deeplx start
-
-      To stop deeplx manually, run:
-        sudo deeplx stop
-
-      To restart deeplx manually, run:
-        sudo deeplx restart
-
-      To start deeplx automatically at login, copy and paste the following command:
-        sudo cp -fv #{etc}/rc.d/deeplx /etc/rc.d/
+      To start, stop, or restart deeplx using brew services, run:
+        brew services start deeplx
+        brew services stop deeplx
+        brew services restart deeplx
     EOS
   end
 
   service do
     name macos: "#{plist_name}"
   end
-      
+
   test do
     system "#{bin}/deeplx", "--version"
   end
