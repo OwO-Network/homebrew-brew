4a1e07ec7f15c67f489c78443faf5d01b42e7a19
missuo <missuo@users.noreply.github.com>
2024-11-30 07:08:51 +0000

    update release version

--- Formula/deeplx.rb
+++ Formula/deeplx.rb
@@ -1,14 +1,14 @@
 class Deeplx < Formula
   desc "DeepLX is a permanently free DeepL API client written in Golang."
   homepage "https://github.com/OwO-Network/DeepLX"
-  version "0.9.8.3"
+  version "0.9.9"
 
   if Hardware::CPU.arm?
     url "https://github.com/OwO-Network/DeepLX/releases/download/v#{version}/deeplx_darwin_arm64"
-    sha256 "2f898c8cca9e2d38cce70767b7617b6e97c1b13c2e3233954c41c32a3caf2108"
+    sha256 "69e3cf9909a5fcb20df57aab9f877fc61d1156e109a8dc5e3cdb89fc4aed053e"
   else
     url "https://github.com/OwO-Network/DeepLX/releases/download/v#{version}/deeplx_darwin_amd64"
-    sha256 "1b0927a138f0057e2488bad76b9495688b46881f364f572bfb429ad7158d13cf"
+    sha256 "6284c1126c8338354dcb89d504d2c45e905636ebbf9e26bc72d934512799eec2"
   end
 
   def install
