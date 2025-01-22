7aca5ea38079e5d9590c444468816e5d13aab036
Vincent Yang <missuo@pm.me>
2025-01-22 15:32:11 -0500

    fix: sed command

--- getRelease.sh
+++ getRelease.sh
@@ -1,8 +1,8 @@
 ###
  # @Author: Vincent Young
  # @Date: 2023-02-17 19:18:08
- # @LastEditors: Vincent Young
- # @LastEditTime: 2023-03-03 02:16:13
+ # @LastEditors: Vincent Yang
+ # @LastEditTime: 2025-01-22 15:30:46
  # @FilePath: /homebrew-brew/getRelease.sh
  # @Telegram: https://t.me/missuo
  # 
@@ -16,7 +16,7 @@ update_deeplx(){
     last_version=$(curl -Ls "https://api.github.com/repos/OwO-Network/DeepLX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/v//g')
 
     # Update the version number in the formula
-    sed -i '' "s/version \".*/version \"${last_version}\"/g" Formula/deeplx.rb
+    sed -i "s/version \".*/version \"${last_version}\"/g" Formula/deeplx.rb
 
     # Download the new binaries
     wget -O deeplx_darwin_amd64 https://github.com/OwO-Network/DeepLX/releases/download/v${last_version}/deeplx_darwin_amd64
@@ -27,8 +27,8 @@ update_deeplx(){
     arm64_sha256=$(sha256sum deeplx_darwin_arm64 | cut -d ' ' -f 1)
 
     # Update the SHA256 hashes in the formula
-    sed -i '' "8s/.*/    sha256 \"${arm64_sha256}\"/" Formula/deeplx.rb
-    sed -i '' "11s/.*/    sha256 \"${amd64_sha256}\"/" Formula/deeplx.rb
+    sed -i "8s/.*/    sha256 \"${arm64_sha256}\"/" Formula/deeplx.rb
+    sed -i "11s/.*/    sha256 \"${amd64_sha256}\"/" Formula/deeplx.rb
 
     # Delete the new binaries
     rm -f deeplx_darwin*
@@ -39,7 +39,7 @@ update_claude2openai(){
     last_version=$(curl -Ls "https://api.github.com/repos/missuo/claude2openai/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed 's/v//g')
 
     # Update version in the formula
-    sed -i '' "s/version \".*\"/version \"${last_version}\"/" Formula/claude2opneai.rb
+    sed -i "s/version \".*\"/version \"${last_version}\"/" Formula/claude2openai.rb
 
     # Download the new binaries
     wget -O claude2openai_darwin_amd64 https://github.com/missuo/claude2openai/releases/download/v${last_version}/claude2openai-darwin-amd64
@@ -50,8 +50,8 @@ update_claude2openai(){
     arm64_sha256=$(sha256sum claude2openai_darwin_arm64 | cut -d ' ' -f 1)
 
     # Update the SHA256 hashes in the formula
-    sed -i '' "7s/.*/    sha256 \"${arm64_sha256}\"/" Formula/claude2opneai.rb
-    sed -i '' "10s/.*/    sha256 \"${amd64_sha256}\"/" Formula/claude2opneai.rb
+    sed -i "7s/.*/    sha256 \"${arm64_sha256}\"/" Formula/claude2openai.rb
+    sed -i "10s/.*/    sha256 \"${amd64_sha256}\"/" Formula/claude2openai.rb
 
     # Delete the new binaries
     rm -f claude2openai_darwin*
