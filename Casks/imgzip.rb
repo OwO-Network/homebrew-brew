b7884e3a31189fd2f928657f76bfef243d9f9dc9
missuo <missuo@users.noreply.github.com>
2025-01-22 23:28:06 +0000

    update release version

--- Casks/imgzip.rb
+++ Casks/imgzip.rb
@@ -2,11 +2,11 @@ cask "imgzip" do
   version "0.0.1"
   
   if Hardware::CPU.arm?
-    sha256 "7059aa27ba0b55afe8642df47ef00511dc57077dfc9f33095aadd2ca7b78abf9"
+    sha256 "15561405b1303e070c53d784bce1d210494e5e78ad281d496561e0be73ae8f21"
     url "https://github.com/missuo/imgzip/releases/download/v#{version}/imgzip-darwin-arm64"
     binary "imgzip-darwin-arm64", target: "imgzip"
   else
-    sha256 "4075e5888153039c15db0132ab0b99d8e28078b6411f73cdf4aa7d08b0ce2d09"
+    sha256 "a498e68d923aa5679ae4ede6bd43d75c51845efbcd93867f51f0760a442d884b"
     url "https://github.com/missuo/imgzip/releases/download/v#{version}/imgzip-darwin-amd64"
     binary "imgzip-darwin-amd64", target: "imgzip"
   end
