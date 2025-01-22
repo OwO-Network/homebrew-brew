1702a8d76f9d8ebfa60bf7c5fd07ac3a75c62bfe
missuo <missuo@users.noreply.github.com>
2025-01-22 20:34:21 +0000

    update release version

--- Formula/claude2openai.rb
+++ Formula/claude2openai.rb
@@ -1,13 +1,13 @@
 class Claude2openai < Formula
   desc "A proxy to convert Claude API into OpenAI API format"
   homepage "https://github.com/missuo/claude2openai"
-  version "1.0.1"
+  version "1.0.3"
   if Hardware::CPU.arm?
     url "https://github.com/missuo/claude2openai/releases/download/v#{version}/claude2openai-darwin-arm64"
-    sha256 "1"
+    sha256 "bb6093e90e8adfe153ccdee6ad0248173063fb0177835ff42d5f67d51082928a"
   else
     url "https://github.com/missuo/claude2openai/releases/download/v#{version}/claude2openai-darwin-amd64"
-    sha256 "1"
+    sha256 "4c936b16fde5a0ea9a5cfdeded6c91631a4ccc7467c10ed10f880304c0297504"
   end
   license "MIT"
 
