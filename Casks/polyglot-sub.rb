ada1f2d13e5560edfce47e59141b6ab73f2c54b7
missuo <missuo@users.noreply.github.com>
2025-02-04 20:03:07 +0000

    update release version

--- Casks/polyglot-sub.rb
+++ Casks/polyglot-sub.rb
@@ -1,6 +1,6 @@
 cask "polyglot-sub" do
   version "1.0"
-  sha256 ""
+    sha256 "28e9e4a09f7d85fdf2a0abfadc7198b4c3bb5dafadefd35b1ffa7a5692b3de8d"
 
   url "https://github.com/missuo/PolyglotSub/releases/download/v#{version}/Polyglot.dmg"
   name "Polyglot Sub"
