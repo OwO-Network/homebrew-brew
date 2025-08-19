6231d89f51659beb63717fdd6df48d0ecf167c5e
missuo <missuo@users.noreply.github.com>
2025-08-19 10:44:31 +0000

    update release version

--- Formula/fixtwitter.rb
+++ Formula/fixtwitter.rb
@@ -1,15 +1,15 @@
 class Fixtwitter < Formula
   desc "Background service that automatically replaces X.com links with fxtwitter.com in clipboard"
   homepage "https://github.com/missuo/fixtwitter"
-  version ""
+  version "1.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/missuo/fixtwitter/releases/download/v1.0.0/fixtwitter-darwin-arm64"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "28f9ca7d83028334c9ea6e70e76c379c4ad61a8fe4ce75145cd385c68ec56b6e"
     else
       url "https://github.com/missuo/fixtwitter/releases/download/v1.0.0/fixtwitter-darwin-amd64"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "ae73606df76b98d617f5da8ceba4db76d86242fe42c85cb8e798fa2bb99f80fe"
     end
   end
 
