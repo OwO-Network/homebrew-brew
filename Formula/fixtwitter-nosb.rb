238262e994e29c789934f26817673e0b451d3dc5
missuo <missuo@users.noreply.github.com>
2025-08-19 10:37:22 +0000

    update release version

--- Formula/fixtwitter-nosb.rb
+++ Formula/fixtwitter-nosb.rb
@@ -1,15 +1,15 @@
 class FixtwitterNosb < Formula
   desc "Background service that automatically replaces X.com links with no.sb in clipboard"
   homepage "https://github.com/missuo/fixtwitter"
-  version "1.0.0"
+  version ""
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/missuo/fixtwitter/releases/download/v1.0.0/fixtwitter-darwin-arm64"
-      sha256 "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d0e1f2"
+      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     else
       url "https://github.com/missuo/fixtwitter/releases/download/v1.0.0/fixtwitter-darwin-amd64"
-      sha256 "b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d0e1f2g3"
+      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     end
   end
 
