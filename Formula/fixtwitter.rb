5b0e0941fd4197d42c01ef6b022a1c766b20a9f3
Vincent Young <missuo@pm.me>
2025-08-19 18:22:21 +0800

    Create fixtwitter.rb

---
+++ Formula/fixtwitter.rb
@@ -0,0 +1,35 @@
+class Fixtwitter < Formula
+  desc "Background service that automatically replaces X.com links with fxtwitter.com in clipboard"
+  homepage "https://github.com/missuo/fixtwitter"
+  version "1.0.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/missuo/fixtwitter/releases/download/v1.0.0/fixtwitter-darwin-arm64"
+      sha256 "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d0e1f2"
+    else
+      url "https://github.com/missuo/fixtwitter/releases/download/v1.0.0/fixtwitter-darwin-amd64"
+      sha256 "b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d0e1f2g3"
+    end
+  end
+
+  def install
+    if Hardware::CPU.arm?
+      bin.install "fixtwitter-darwin-arm64" => "fixtwitter"
+    else
+      bin.install "fixtwitter-darwin-amd64" => "fixtwitter"
+    end
+  end
+
+  service do
+    run [opt_bin/"fixtwitter"]
+    keep_alive true
+    log_path var/"log/fixtwitter.log"
+    error_log_path var/"log/fixtwitter.log"
+    process_type :background
+  end
+
+  test do
+    system "#{bin}/fixtwitter", "-h"
+  end
+end
