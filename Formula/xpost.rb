1a7eedcf78259026afa0323beec523d9ce427193
missuo <missuo@users.noreply.github.com>
2026-02-10 11:32:10 +0000

    update release version

--- Formula/xpost.rb
+++ Formula/xpost.rb
@@ -1,13 +1,13 @@
 class Xpost < Formula
   desc "A command-line tool and HTTP API for posting to X (Twitter)"
   homepage "https://github.com/missuo/xpost"
-  version "0.0.5"
+  version "0.0.6"
   if Hardware::CPU.arm?
     url "https://github.com/missuo/xpost/releases/download/v#{version}/xpost-darwin-arm64"
-    sha256 "567c7180ce7d1002024ed7f4aa1c4aa8075c8e3101f115d61e0c6f754e64fe3a"
+    sha256 "d9f2fa0b1a0948cfe0a9cb25389c7a86465b6dadb53075ffdefaba5b5cda92c4"
   else
     url "https://github.com/missuo/xpost/releases/download/v#{version}/xpost-darwin-amd64"
-    sha256 "b68b253e345edaf530e80130b5c9f09fcdd77eede8fc17ea922180adaa6a2cf6"
+    sha256 "6798abc263a2d6191b691ec982bd7cc4cb4b23f664429236b37d58ea3ea8ddcf"
   end
   license "Apache-2.0"
 
