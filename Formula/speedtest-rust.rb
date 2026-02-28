ccdf2921b8bec686bb6c990774db8716731a0ff4
missuo <missuo@users.noreply.github.com>
2026-02-28 14:03:59 +0000

    update release version

--- Formula/speedtest-rust.rb
+++ Formula/speedtest-rust.rb
@@ -1,13 +1,13 @@
 class SpeedtestRust < Formula
   desc "Speedtest CLI powered by Apple CDN, written in Rust"
   homepage "https://github.com/missuo/speedtest-rust"
-  version "1.0.4"
+  version "1.0.5"
   if Hardware::CPU.arm?
     url "https://github.com/missuo/speedtest-rust/releases/download/v#{version}/speedtest-darwin-arm64"
-    sha256 "480641b0cb8d5b9a9439372e8bc339882cc29de5b82fd8f29c26b548021c44da"
+    sha256 "ac526f62615941920f73ff6db5b1507c18dbc278bd05aeb7891647bff136adae"
   else
     url "https://github.com/missuo/speedtest-rust/releases/download/v#{version}/speedtest-darwin-amd64"
-    sha256 "f670a16f3de2535627b5e9a39c55a318488a2d1028a43e584950004af9823bfe"
+    sha256 "7ec10a748ee80030daad77ad83f0a7752f20d0c0c25c4da210083a270bf1145d"
   end
   license "MIT"
 
