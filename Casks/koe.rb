2d14d9c02d2492911228de9d8fbe9d02389aca4e
missuo <55200481+missuo@users.noreply.github.com>
2026-03-22 01:48:51 +0000

    update release version

--- Casks/koe.rb
+++ Casks/koe.rb
@@ -1,6 +1,6 @@
 cask "koe" do
-  version "1.0.3"
-  sha256 "5451a64d4759d01fa9aaa4e992a9b51caa7315d6e236b8e105cc46045b8d9f6e"
+  version "1.0.4"
+  sha256 "219302844aea1aaf7f63bd3fc31b77c9170045c4ff3c1e2ae7f8d48346294454"
 
   url "https://github.com/missuo/koe/releases/download/v#{version}/Koe-macOS-arm64.zip"
   name "Koe"
