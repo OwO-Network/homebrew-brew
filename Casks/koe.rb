5e95709ed072755e2e7bba2934ecfe1fb5cbae3d
Vincent Yang <missuo@pm.me>
2026-04-08 02:10:39 -0700

    Update koe.rb

--- Casks/koe.rb
+++ Casks/koe.rb
@@ -1,6 +1,6 @@
 cask "koe" do
   version "1.0.14"
-  sha256 "89be48005dcaf75dcfdf2f4c8d0b4eb5674058b04fbf812d7a41a1a649697795"
+  sha256 "814f7e3930bc1c7a801274369770b692aafbb86f86dc54401dee71a49f1c609c"
 
   url "https://github.com/missuo/koe/releases/download/v#{version}/Koe-macOS-arm64.zip"
   name "Koe"
