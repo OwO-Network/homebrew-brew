77f940c1297ad665464a85bf9890f3fb77985c8d
Vincent Yang <missuo@pm.me>
2026-08-23 20:14:37 -0700

    chore(kumone): bump to 0.2.4

--- Casks/kumone.rb
+++ Casks/kumone.rb
@@ -1,6 +1,6 @@
 cask "kumone" do
-  version "0.2.3"
-  sha256 "faa602efa781678a2119f6217b1b91d637405a6f11047abf77e8f9a1af793373"
+  version "0.2.4"
+  sha256 "388f6174bb4c55ee9aa6c049aaf7b0a6e5311ad26d60bfaafc1358da16692f33"
 
   url "https://github.com/missuo/kumone/releases/download/v#{version}/Kumone-#{version}.zip"
   name "Kumone"
